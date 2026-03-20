555d7ef3c4ed0c90e6bc434f76933818aafd1708
snayusr <52749148+snayusr@users.noreply.github.com>
2026-03-20 16:43:02 +0800

    Update [filename].ts

--- functions/uploads/[filename].ts
+++ functions/uploads/[filename].ts
@@ -1,16 +1,14 @@
-type Bindings = {
-  DB: D1Database;
-};
+type Bindings = { DB: D1Database; };
 
 export const onRequest: PagesFunction<Bindings> = async (context) => {
   const filename = context.params.filename as string;
-  if (!filename) return new Response("Not Found", { status: 404 });
+  const url = `/uploads/${filename}`;
 
   const media = await context.env.DB.prepare("SELECT data, type FROM album_media WHERE url = ?")
-    .bind(`db-data:${filename}`)
+    .bind(url)
     .first<{ data: ArrayBuffer, type: string }>();
 
-  if (!media || !media.data) return new Response("Image Not Found", { status: 404 });
+  if (!media || !media.data) return new Response("Not Found", { status: 404 });
 
   return new Response(media.data, {
     headers: {
